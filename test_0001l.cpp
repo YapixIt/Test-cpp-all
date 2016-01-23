c51b70c664a8938868448fdd2c1e71a3e939b423
Mathias Garnier - YapixIt <YapixIt@users.noreply.github.com>
2016-01-23 19:02:40 +0100

    Create test_0001l.cpp

---
+++ test_0001l.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+		Application Crash test !
+*/
+
+int main()
+{
+	srand(time(NULL));
+	int a = (rand() % 64) + 1;
+
+	int x[-a];
+	cout << x;
+	//Application Crash ;)
+}
