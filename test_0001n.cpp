74f650f9b08834eacf47712ea63ad0cda5ceaafd
Mathias Garnier - YapixIt <YapixIt@users.noreply.github.com>
2016-01-23 19:33:44 +0100

    Create test_0001n.cpp

---
+++ test_0001n.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+	  NONE result test !
+*/
+
+int main()
+{
+	srand(time(NULL));
+	int a = (rand() % 1) + 1;
+
+	for(int i = a; i < a - 1; i++)
+	{
+	cout << "Infinite generator in a for expression !"<< endl;
+	//result(NONE)
+	}
+}
