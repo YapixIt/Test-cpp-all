136681d1ce03d8b4d22c762e44a8045da3450958
Mathias Garnier - YapixIt <YapixIt@users.noreply.github.com>
2016-01-23 15:11:32 +0100

    Create test_0001f.cpp

---
+++ test_0001f.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int x_0 = (rand() % 64) + 1;
+	vector<int> x_v[x_0][x_0][x_0];
+	for (int k = 0; k < 3; k++)
+	{
+		for (int k = 0; k < 2; k++)
+		{
+			for (int k = 0; k < 1; k++)
+			{
+				cout << x_v;
+				//Aleatory result ^^ ;)
+			}
+		}
+	}
+}
