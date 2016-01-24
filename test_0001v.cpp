cb196dbec062e5851cfade9c61e6760fdf67f01d
Mathias Garnier - YapixIt <YapixIt@users.noreply.github.com>
2016-01-24 15:33:33 +0100

    Create test_0001v.cpp

---
+++ test_0001v.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+		Cowsay !
+*/
+
+int main()
+{
+	string str;
+
+	std::cout << "Enter text !" << std::endl;
+	std::cin >> str;
+
+	std::cout << str << std::endl;
+	std::cout << "^__^ " << std::endl;
+    std::cout << "(oo)\_______ " << std::endl;
+    std::cout << "(__)\       )\/\ " << std::endl;
+    std::cout << "    ||----w | " << std::endl;
+    std::cout << "    ||     || " << std::endl;
+}
