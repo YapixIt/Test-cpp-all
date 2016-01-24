61155aab34faf29b1a93d2bf61a3cba31897037b
Mathias Garnier - YapixIt <YapixIt@users.noreply.github.com>
2016-01-24 15:17:52 +0100

    Update test_0001u.cpp

--- test_0001u.cpp
+++ test_0001u.cpp
@@ -20,5 +20,5 @@ int main()
     int t_0 = r_0, t_1 = r_1, t_2 = r_2, t_3 = r_3, t_4 = r_4, t_5 = r_5;
 
     std::cout << "Number choice ->" << t_0 << ", " << t_1 << ", " << t_3 << ", " << t_4 << ", " << t_5 << " ."<< std::endl;
-  //First result(Number choice ->4285984, -2, 2752248, 4285984, 64)
+  //First random result(Number choice ->4285984, -2, 2752248, 4285984, 64)
   }
