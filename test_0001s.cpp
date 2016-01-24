9f6ada932c887fa4d2002dcb39b161739b230cbc
Mathias Garnier - YapixIt <YapixIt@users.noreply.github.com>
2016-01-24 13:57:26 +0100

    Create test_0001s.cpp

---
+++ test_0001s.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+		Application Crash test !
+*/
+
+int main()
+{
+	int a = 4;
+
+	int x[-a];
+	cout << &x;
+	//For all integer >= 5(Application crash) ^^
+}
